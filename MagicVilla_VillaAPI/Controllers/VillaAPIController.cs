3558dfea3d33e919c5527105e247efa47c9a1b7c
Juan Cruz Luna <juancruz.luna@fiserv.com>
2023-06-19 21:03:54 +0000

    Endpoints/DTO/DATA ( Section 2)

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() 
+        {
+            return VillaStore.villalist;
+            
+        }
+
+        [HttpGet("id")]
+        public VillaDTO GetVilla( int id)
+        {
+            return VillaStore.villalist.FirstOrDefault(u=>u.Id==id);
+
+        }
+
+
+    }
+}
