6297d7875e22c71028999b4717877c47e352daa0
Juan Cruz Luna <juancruz.luna@fiserv.com>
2024-06-13 20:44:29 -0700

    Small change in VillaController.cs

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -46,7 +46,7 @@ namespace MagicVilla_VillaAPI.Controllers
             
         }
 
-        [HttpGet("{id:int}", Name = "GetVillaName")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
